60c6929929e3463ff0af500cc64c93c82c8352ee
Tilen Komel <me@komelt.dev>
2026-02-18 18:14:21 +0100

    Update

--- gen-local-certs.sh
+++ gen-local-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 NAME=meshtrail
-DOMAIN=meshtrail.meshnet.si # Use your own domain name
+DOMAIN=test.meshtrail.si # Use your own domain name
 
 mkdir -p "letsencrypt/live/${DOMAIN}/"
 cd "letsencrypt/live/${DOMAIN}/"
